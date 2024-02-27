ae340104ed630e41a74d390f16fd4e64e67c8473
Dion Tryban <github@diontryban.com>
2024-02-27 16:48:03 -0500

    Add emoji picker

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -1,12 +1,17 @@
-extends CharacterBody2D
-
+class_name Player extends CharacterBody2D
 
 const SPEED = 300.0
 
+@onready var emoji: Sprite2D = $Emoji
 
-func _physics_process(delta):
+
+func _physics_process(_delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
 	velocity = direction * SPEED
 	
 	move_and_slide()
+
+
+func set_emoji(texture: Texture2D):
+	emoji.texture = texture
