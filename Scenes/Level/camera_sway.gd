9b40910f614256478349ac851a3b1e2b7d45c51f
user <user@FIH-COM23a104.fsu.edu>
2024-02-28 15:44:27 -0500

    Remove camera sway

--- Scenes/Level/camera_sway.gd
+++
@@ -1,8 +0,0 @@
-extends Camera2D
-
-var time: float = 0
-
-
-func _process(delta):
-	time += delta
-	rotation = sin(time / 3) * 0.02
--- Scenes/Level/level.gd
+++ Scenes/Level/level.gd
@@ -3,15 +3,10 @@ extends Node2D
 var player_scene: PackedScene = load("res://Entities/Player/player.tscn")
 
 
-func enable():
-	visible = true
-	$UILayer.visible = true
-
-
 func spawn_player(nickname: String, color: Color):
 	var player: Player = player_scene.instantiate()
 	player.set_nickname.call_deferred(nickname)
 	player.set_color.call_deferred(color)
 	player.global_position = get_viewport().get_visible_rect().size / 2
-	$Island/Players.add_child(player)
+	$Players.add_child(player)
 	%EmojiPicker.player = player
