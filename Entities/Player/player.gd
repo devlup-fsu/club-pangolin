a92e2a685b0fd3e11536280bf81506f1d0468c94
alectrem <tremblayalec14@gmail.com>
2024-02-28 01:27:40 -0500

    adds groovin and pangolin spectrality, centers line edit text on select screen

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -6,6 +6,8 @@ const SPEED = 300.0
 @onready var bottom_sprite: Sprite2D = $BottomSprite
 @onready var nickname_label: Label = $NicknameLabel
 
+var is_groovin: bool = false
+var time: float = 0
 
 func _physics_process(_delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -13,7 +15,6 @@ func _physics_process(_delta):
 	velocity = direction * SPEED
 	
 	move_and_slide()
-
 
 func set_nickname(value: String):
 	nickname_label.text = value
@@ -25,3 +26,13 @@ func set_color(color: Color):
 
 func set_emoji(texture: Texture2D):
 	emoji.texture = texture
+
+
+# groovin, baby
+func _process(delta):
+	time += delta
+	if is_groovin:
+		var groove = pow(sin(time * 7), 3) * 0.2
+		for sprite in [%BottomSprite, %TopSprite]:
+			sprite.scale.y = groove * -0.05 + 0.1
+			sprite.position.y = groove * 20
