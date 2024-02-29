5d920d5384eb387e0f565712cab5eddc80c1b1e7
Dion Tryban <github@diontryban.com>
2024-02-28 22:01:15 -0500

    Simplify multiplayer script

--- Autoloads/multiplayer.gd
+++ Autoloads/multiplayer.gd
@@ -1,25 +1,15 @@
 extends Node
 
 
-func start_server(port: int) -> bool:
+func start_server(port: int):
 	var peer = ENetMultiplayerPeer.new()
 	peer.create_server(port)
 	
-	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
-		OS.alert("Failed to start multiplayer server.")
-		return false
-	
 	multiplayer.multiplayer_peer = peer
-	return true
 
 
-func start_client(address: String, port: int) -> bool:
+func start_client(address: String, port: int):
 	var peer = ENetMultiplayerPeer.new()
 	peer.create_client(address, port)
 	
-	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
-		OS.alert("Failed to start multiplayer client.")
-		return false
-	
 	multiplayer.multiplayer_peer = peer
-	return true
