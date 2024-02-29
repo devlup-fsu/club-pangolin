dced0682614cc88e03ff4fced3be4c6f1315bd58
Dion Tryban <github@diontryban.com>
2024-02-28 23:20:46 -0500

    Fix

--- Autoloads/multiplayer.gd
+++ Autoloads/multiplayer.gd
@@ -6,6 +6,11 @@ func start_server(port: int):
 	peer.create_server(port)
 	
 	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(_on_multiplayer_peer_connected)
+
+
+func _on_multiplayer_peer_connected(id: int):
+	print(id, " connected")
 
 
 func start_client(address: String, port: int):
